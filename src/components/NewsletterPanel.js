79dd742de8adcf3e4b5d3c3ee25bfb05ab6b581e
Ryan Romano <ryanaromano@gmail.com>
2017-12-14 21:30:27 -0500

    newsletterpanel posts to api

--- src/components/NewsletterPanel.js
+++ src/components/NewsletterPanel.js
@@ -5,6 +5,7 @@ import '../styles/Home.css';
 export default class NewsletterPanel extends React.Component {
   constructor(props) {
     super(props);
+    this.makePostRequest = this.makePostRequest.bind(this);
   }
 
   render() {
@@ -12,10 +13,27 @@ export default class NewsletterPanel extends React.Component {
       <div className="section-newsletter">
         <h2>SUBSCRIBE TO NEWSLETTER</h2>
         <form action="">
-          <input type="text" name="email" placeholder="Your Email"/>
-          <input type="submit" value="Subscribe"/>
+          <input type="text" ref="email" name="email" placeholder="Your Email"/>
+          <input type="submit" onClick={this.makePostRequest} value="Subscribe" onSubmit="return false"/>
         </form>
       </div>
     );
   }
+
+  makePostRequest() {
+    var data = "email=" + this.refs.email.value;
+    
+    var xhr = new XMLHttpRequest();
+    
+    xhr.addEventListener("readystatechange", function () {
+        console.log(this.responseText);
+        alert(this.responseText);
+    });
+    
+    xhr.open("POST", "http://dev3.apppartner.com/Reactors/scripts/add-email.php");
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    
+    xhr.send(data);
+  }
+
 }
