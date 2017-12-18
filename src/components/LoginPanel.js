0111e161621691857c43b850ab3f3566ba995d5c
Ryan Romano <ryanaromano@gmail.com>
2017-12-18 12:41:51 -0500

    endpoint functions working

--- src/components/LoginPanel.js
+++ src/components/LoginPanel.js
@@ -11,6 +11,8 @@ export default class LoginPanel extends React.Component {
     };
     this.login = this.login.bind(this);
     this.signup = this.singup.bind(this);
+    this.makeLoginRequest = this.makeLoginRequest.bind(this);
+    this.makeSignupRequest = this.makeSignupRequest.bind(this);
   }
 
   renderLoginState(){
@@ -19,13 +21,13 @@ export default class LoginPanel extends React.Component {
         <div className="credential-field">
           <div className="toggle-login">
             <a href="javascript:void(0)" onClick={this.signup}>SIGN UP</a>
-            <a href="javascript:void(0)" class="active" onClick={this.login}>LOGIN</a>
+            <a href="javascript:void(0)" className="active" onClick={this.login}>LOGIN</a>
           </div>
           <div className="sign-up-form">
-            <form action="">
-              <input className="username" type="text" name="email" placeholder="Email"/>
-              <input className="password" type="text" name="password" placeholder="Password"/>
-              <button type="submit" value={this.buttonMessage}>LOG IN</button>
+            <form onSubmit={this.makeLoginRequest} >
+              <input className="username" ref="loginemail"type="text" name="email" placeholder="Email"/>
+              <input className="password" ref="loginpass" type="text" name="password" placeholder="Password"/>
+              <button type="submit" value={this.buttonMessage} >LOG IN</button>
             </form>
           </div>
         </div>
@@ -34,44 +36,20 @@ export default class LoginPanel extends React.Component {
     return(
       <div className="credential-field">
         <div className="toggle-login">
-          <a href="javascript:void(0)" class="active" onClick={this.signup}>SIGN UP</a>
+          <a href="javascript:void(0)" className="active" onClick={this.signup}>SIGN UP</a>
           <a href="javascript:void(0)" onClick={this.login}>LOGIN</a>
         </div>
         <div className="sign-up-form">
-          <form action="">
-            <input className="username" type="text" name="username" placeholder="Username"/>
-            <input className="email" type="text" name="email" placeholder="Email"/>
-            <input className="password" type="text" name="password" placeholder="Password"/>
+          <form onSubmit={this.makeSignupRequest}>
+            <input className="username" ref="signupuser" type="text" name="username" placeholder="Username"/>
+            <input className="email" ref="signupemail" type="text" name="email" placeholder="Email"/>
+            <input className="password" ref="signuppass" type="text" name="password" placeholder="Password"/>
             <button type="submit" value={this.buttonMessage}>SIGN UP</button>
           </form>
         </div>
       </div>
     );
   }
-
-  // componentWillMount() {;
-  //   if (typeof values.someString === 'undefined') {
-  //     debugger;
-  //   } else {
-  //     debugger;
-  //   }
-  // }
-  // componentDidMount() {
-  //   const {values} = this.props;
-  //   if (typeof values.someString === 'undefined') {
-  //     debugger;
-  //   } else {
-  //     debugger;
-  //   }
-  // }
-  // componentWillReceiveProps(newProps) {
-  //   const {values} = newProps;
-  //   if (typeof values.someString === 'undefined') {
-  //     debugger;
-  //   } else {
-  //     debugger;
-  //   }
-  // }
 
   render() {
     return (
@@ -88,5 +66,47 @@ export default class LoginPanel extends React.Component {
   singup() {
     this.setState({ isLogin: false });    
     this.setState({ buttonMessage: "SIGN UP"});
+  }
+
+  makeLoginRequest(event) {
+    event.preventDefault();
+    var data = "email=" + this.refs.loginemail.value + "&password=" + this.refs.loginpass.value;
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        if(this.status === 404 || this.status === 401) {
+          alert("Something went wrong. Please re-enter your login information.");
+        } else {
+          var body = JSON.parse(this.response);
+          if(body.user_username === undefined) { 
+            alert("Welcome back!");
+          } else {
+            alert("Welcome back, " + body.user_username + ".")
+          }
+        }
+      }
+    });
+    xhr.open("POST", "http://dev3.apppartner.com/Reactors/scripts/user-login.php");
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    xhr.send(data);
+  }
+
+  makeSignupRequest(event) {
+    event.preventDefault();    
+    var data ="username=" + this.refs.signupuser.value + "&email=" + this.refs.signupemail.value + "&password=" + this.refs.signuppass.value;
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        var body = JSON.parse(this.response);        
+        if(this.status === 400) {
+          alert(body.message);      
+        } else {
+          alert("Signed in successfully. Your user id is: " + body.user_id);
+        }
+      }
+    });
+    xhr.open("POST", "http://dev3.apppartner.com/Reactors/scripts/user-signup.php");
+    xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+    xhr.send(data);
   }
 }
--- src/components/NewsletterPanel.js
+++ src/components/NewsletterPanel.js
@@ -12,28 +12,35 @@ export default class NewsletterPanel extends React.Component {
     return (
       <div className="section-newsletter">
         <h2>SUBSCRIBE TO NEWSLETTER</h2>
-        <form action="">
-          <input type="text" ref="email" name="email" placeholder="Your Email"/>
-          <input type="submit" onClick={this.makePostRequest} value="Subscribe" onSubmit="return false"/>
+        <form onSubmit={this.makePostRequest}>
+          <input type="text" ref="subscribe" placeholder="Your Email"/>
+          <input type="submit" value="Subscribe"/>
         </form>
       </div>
     );
   }
 
-  makePostRequest() {
-    var data = "email=" + this.refs.email.value;
-    
+  makePostRequest(event) {
+    event.preventDefault();    
+    var email = this.refs.subscribe.value;
+    var data = "email=" + email;
     var xhr = new XMLHttpRequest();
-    
     xhr.addEventListener("readystatechange", function () {
-        console.log(this.responseText);
-        alert(this.responseText);
+      if (this.readyState === 4) {
+        var body = JSON.parse(this.response);
+        this.validateEmail = function(useremail) {
+          var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return re.test(useremail.toLowerCase());
+        }
+        if(this.validateEmail(email)){
+          alert(body.message);
+        } else {
+          alert("Enter a valid email to subscribe to our newsletter.");
+        }
+      }
     });
-    
     xhr.open("POST", "http://dev3.apppartner.com/Reactors/scripts/add-email.php");
     xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
-    
     xhr.send(data);
   }
-
 }
